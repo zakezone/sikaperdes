118aa53b72ae6a79496dcaf83d8afc863b3c026c
zakezone <zakezone69@gmail.com>
2023-10-16 14:56:42 +0700

    init REST API controller JWT 1 HOURS Token

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -147,7 +147,8 @@ $routes->get('/user/data/kawasan', 'User\Data::verifikasi_data_kawasan', ['filte
 $routes->match(['get', 'post'], '/user/data/load_data_kawasan', 'User\Data::load_data_kawasan', ['filter' => 'authusersie']);
 
 // API SIKAPERDES
-$routes->match(['get', 'post'], '/api/auth', 'Api\Auth::index');
+$routes->post('/api/auth', 'Api\Auth::index');
+$routes->get('/api/kawasan', 'Api\Kawasan::index');
 // $routes->group('api', ['namespace' => 'App\Controllers\Api'], static function ($routes) {
 //     $routes->resource('Auth');
 //     $routes->resource('Kawasan');
